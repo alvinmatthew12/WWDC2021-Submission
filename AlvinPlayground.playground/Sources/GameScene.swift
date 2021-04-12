e73b2a3c7dffbd95dbfc141633175918b2ce9113
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2021-04-12 22:06:47 +0700

    Create inventory collection view

--- AlvinPlayground.playground/Sources/GameScene.swift
+++ AlvinPlayground.playground/Sources/GameScene.swift
@@ -4,12 +4,13 @@ import SpriteKit
 public class GameScene: SKScene, SKPhysicsContactDelegate {
 
     let player = Player()
-    var inventoryView = InventoryView()
+    var inventoryView: InventoryView?
     var goldStone: Stone?
     var lithiumStone: Stone?
     var sceneSize: CGSize
     
     var inventory: [String] = []
+    var isInventoryOpen: Bool = false
     
     public override init(size: CGSize) {
         sceneSize = size
@@ -30,7 +31,7 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(Ceiling(size: sceneSize))
         self.addChild(player)
         setupStones()
-        setupInventory()
+        setupInventoryButton()
     }
     
     func setupStones() {
@@ -51,26 +52,33 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(goldStone!)
     }
     
-    func setupInventory() {
+    func setupInventoryButton() {
         let inventoryButton = InventoryImageButton(frame: CGRect(x: 20, y: (self.view?.frame.height)! - 60, width: 40, height: 40))
         inventoryButton.addTarget(self, action: #selector(inventoryButtonAction), for: .touchUpInside)
         self.view?.addSubview(inventoryButton)
-        
-        inventoryView = InventoryView(frame: CGRect(x: (self.view?.bounds.midX)!, y: (self.view?.bounds.midX)!, width: 500, height: 400))
-        inventoryView.center = self.view!.center
-        inventoryView.isHidden = true
-        self.view?.addSubview(inventoryView)
     }
     
     @objc func inventoryButtonAction(sender : UIButton) {
-        inventoryView.isHidden = !inventoryView.isHidden
+        if inventoryView?.isDescendant(of: self.view!) == true {
+            inventoryView?.removeFromSuperview()
+            isInventoryOpen = false
+        } else {
+            setupInventory()
+            isInventoryOpen = true
+        }
+    }
+    
+    func setupInventory() {
+        inventoryView = InventoryView(frame: CGRect(x: (self.view?.bounds.midX)!, y: (self.view?.bounds.midX)!, width: 500, height: 400), pInventory: inventory)
+        inventoryView?.center = self.view!.center
+        self.view?.addSubview(inventoryView!)
     }
     
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first!
         let location = touch.location(in: self)
         
-        if inventoryView.isHidden == true {
+        if inventoryView == nil || inventoryView?.isDescendant(of: self.view!) == false {
             checkTouchedNode(location)
         }
     }
@@ -79,7 +87,7 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
         let touch = touches.first!
         let location = touch.location(in: self)
         
-        if inventoryView.isHidden == true {
+        if inventoryView == nil || inventoryView?.isDescendant(of: self.view!) == false {
             player.movePlayer(location, frame)
         }
     }
@@ -133,11 +141,32 @@ public class InventoryImageButton: UIButton {
 
 // MARK:- InventoryView
 
-
-public class InventoryView: UIView {
+public class InventoryView: UIView, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    public override init(frame: CGRect) {
+    public var inventory: [String] = []
+    
+    var collectionViewContainer = UIView()
+    var imageViewContainer = UIView()
+    var imageView = UIImageView()
+    var shortDescContainer = UIView()
+    var descTextView = UITextView()
+    var collectionView: UICollectionView!
+    
+    var selectedItems: String?
+    
+    public init(frame: CGRect, pInventory: [String]) {
         super.init(frame: frame)
+        
+        for i in 0...23 {
+            if pInventory.indices.contains(i) {
+                inventory.append(pInventory[i])
+            } else {
+                inventory.append("")
+            }
+        }
+        
+        imageView.image = UIImage(named: inventory[0])
+        selectedItems = inventory[0]
         
         self.backgroundColor = .white
         self.layer.masksToBounds = true
@@ -164,18 +193,131 @@ public class InventoryView: UIView {
         closeButton.translatesAutoresizingMaskIntoConstraints = false
         closeButton.addTarget(self, action: #selector(close), for: .touchUpInside)
         
+        collectionViewContainer.backgroundColor = .gray
+        self.addSubview(collectionViewContainer)
+        collectionViewContainer.topAnchor.constraint(equalTo: self.topAnchor, constant: 56).isActive = true
+        collectionViewContainer.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20).isActive = true
+        collectionViewContainer.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        collectionViewContainer.heightAnchor.constraint(equalToConstant: 320).isActive = true
+        collectionViewContainer.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageViewContainer.backgroundColor = .gray
+        self.addSubview(imageViewContainer)
+        imageViewContainer.topAnchor.constraint(equalTo: self.topAnchor, constant: 56).isActive = true
+        imageViewContainer.leadingAnchor.constraint(equalTo: collectionViewContainer.trailingAnchor, constant: 20).isActive = true
+        imageViewContainer.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        imageViewContainer.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        imageViewContainer.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.contentMode = .scaleAspectFit
+        self.addSubview(imageView)
+        imageView.topAnchor.constraint(equalTo: imageViewContainer.topAnchor, constant: 5).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: imageViewContainer.leadingAnchor, constant: 5).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: imageViewContainer.trailingAnchor, constant: -5).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: imageViewContainer.bottomAnchor, constant: -5).isActive = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        shortDescContainer.backgroundColor = .gray
+        self.addSubview(shortDescContainer)
+        shortDescContainer.topAnchor.constraint(equalTo: self.topAnchor, constant: 56).isActive = true
+        shortDescContainer.leadingAnchor.constraint(equalTo: imageViewContainer.trailingAnchor, constant: 20).isActive = true
+        shortDescContainer.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20).isActive = true
+        shortDescContainer.widthAnchor.constraint(equalToConstant: 220).isActive = true
+        shortDescContainer.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        shortDescContainer.translatesAutoresizingMaskIntoConstraints = false
+        
+        descTextView.backgroundColor = .gray
+        self.addSubview(descTextView)
+        descTextView.topAnchor.constraint(equalTo: imageViewContainer.bottomAnchor, constant: 20).isActive = true
+        descTextView.leadingAnchor.constraint(equalTo: collectionViewContainer.trailingAnchor, constant: 20).isActive = true
+        descTextView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20).isActive = true
+        descTextView.widthAnchor.constraint(equalToConstant: 330).isActive = true
+        descTextView.heightAnchor.constraint(equalToConstant: 220).isActive = true
+        descTextView.translatesAutoresizingMaskIntoConstraints = false
+        
+        setupCollectionView()
     }
     
     @objc func close(sender: UIButton) {
-        self.isHidden = true
+        self.removeFromSuperview()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func setupCollectionView() {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: 39, height: 39)
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+
+        collectionView = UICollectionView(frame: collectionViewContainer.frame, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(InventoryCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        
+        collectionViewContainer.addSubview(collectionView)
+        collectionView.topAnchor.constraint(equalTo: collectionViewContainer.topAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: collectionViewContainer.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: collectionViewContainer.trailingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: collectionViewContainer.bottomAnchor).isActive = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return inventory.count
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! InventoryCollectionViewCell
+        cell.backgroundColor = .gray
+        
+        let inventoryName = inventory[indexPath.item]
+        cell.imageView?.image = UIImage(named: inventoryName)
+        if inventoryName == selectedItems && inventoryName != "" {
+            cell.backgroundColor = .systemTeal
+        }
+        return cell
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let inventoryName = inventory[indexPath.item]
+        if inventoryName != "" {
+            self.imageView.image = UIImage(named: inventoryName)
+            self.selectedItems = inventoryName
+            self.collectionView.reloadData()
+        }
+    }
 }
 
+public class InventoryCollectionViewCell: UICollectionViewCell {
+    var imageView: UIImageView?
+    var label: UILabel?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        imageView = UIImageView(frame: self.bounds)
+        imageView!.contentMode = .scaleAspectFit
+        contentView.addSubview(imageView!)
+        imageView!.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5).isActive = true
+        imageView!.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5).isActive = true
+        imageView!.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5).isActive = true
+        imageView!.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5).isActive = true
+        imageView!.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override var bounds: CGRect {
+        didSet {
+            contentView.frame = bounds
+        }
+    }
+}
 
 // MARK:- PlayerNode
 
